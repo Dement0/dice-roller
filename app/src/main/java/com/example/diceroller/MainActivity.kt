1c4a9f78f26ad52dc71a464e43813567d512321f
Dement0 <ilkeer.ozgur@gmail.com>
2019-10-11 17:51:18 +0200

    Add dice images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,12 +15,31 @@ class MainActivity : AppCompatActivity() {
         // Get button with the id roll_button
         val rollButton : Button = findViewById(R.id.roll_button)
 
-        // Change the inner text of the button
-        rollButton.text = "Let's Roll"
-
-        // Show a toast when button is clicked
+        // Call rollDice function when clicked
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+
+        // Get a random integer between 1 and 7
+        val randomInt : Int = Random().nextInt(6) + 1
+
+        // Get a reference to the ImageView and assign it to diceImage
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+
+        // Choose the right drawable resource based on the value of randomInt
+        val drawableResource =  when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Set the image to the corresponding drawableResource
+        diceImage.setImageResource(drawableResource)
     }
 }
